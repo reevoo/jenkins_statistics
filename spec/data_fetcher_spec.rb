ed88704a747c337295f48d35cae163929c89f62f
Ionut Alexandru Anca <ionut998@gmail.com>
2015-12-06 21:56:51 +0000

    more specs for DataFetcher

--- spec/data_fetcher_spec.rb
+++ spec/data_fetcher_spec.rb
@@ -1,7 +1,5 @@
 require 'spec_helper'
-
 require 'data_fetcher'
-
 
 describe DataFetcher do
   describe '.http_get' do
@@ -32,6 +30,51 @@ describe DataFetcher do
 
       builds = DataFetcher.new('project_1').all_builds
       expect(builds).to eq(response_data['builds'])
+    end
+  end
+
+  describe '#all_data' do
+    let(:build_data_example) do
+      {report_1: 'data_1'}
+    end
+    it 'returns an array of data about each build' do
+      allow_any_instance_of(DataFetcher)
+        .to receive(:all_builds).and_return([{'url' => 'http://example.com/build1/'}])
+
+      allow(DataFetcher)
+        .to receive(:http_get).with('http://example.com/build1/api/json')
+            .and_return(build_data_example)
+
+      all_data = DataFetcher.new('project_1').all_data
+      expect(all_data).to eq([{report_1: 'data_1'}])
+    end
+  end
+
+  describe '#all_builds_detailed' do
+    let(:detailed_build_data_json) do
+      {spec1: {time_taken_to_run: 2}}
+    end
+    let(:build_data_example) do
+      {'url' => 'http://example.com/build1/','build_1' => 'data_1'}
+    end
+    it 'returns an array of detailed data about each build' do
+     allow_any_instance_of(DataFetcher)
+        .to receive(:all_data).and_return([build_data_example])
+
+      allow(DataFetcher)
+        .to receive(:http_get).with('http://example.com/build1/artifact/rspec.json')
+            .and_return(detailed_build_data_json)
+
+      all_builds_detailed = DataFetcher.new('project_1').all_builds_detailed
+      expect(all_builds_detailed).to eq(
+        [
+          {
+            'url' => 'http://example.com/build1/',
+            'build_1' => 'data_1', 
+            :detailed_output => detailed_build_data_json
+          }
+        ]
+      )
     end
   end
 end
