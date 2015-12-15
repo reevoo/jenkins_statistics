31a1c73b51f294fd2c8937eacbc55396b3cddd0a
Ionut Alexandru Anca <ionut998@gmail.com>
2015-12-15 13:07:03 +0000

    make projects reports more granular

--- lib/jenkins_statistics.rb
+++ lib/jenkins_statistics.rb
@@ -13,23 +13,29 @@ require './lib/reports/ci_time_broken_report'
 class JenkinsStatistics
   
   def self.generate
-    ENV.fetch('BRIEF_REPORT_FOR').split(',').each do |project|
-      all_builds = DataFetcher.new(project).all_data.first(100)
+    # TODO remove duplication and find a way of accessing ci only one per project
 
+    ENV.fetch('TIME_BROKEN_REPORT_PROJECTS').split(',').each do |project|
+      all_builds = DataFetcher.new(project).all_data.first(ENV.fetch('NUMBER_OF_BUILDS_TO_ANALIZE').to_i)
       report = CITimeBrokenReport.new(project, all_builds.dup)
       report.present
+    end
 
+    ENV.fetch('AVERAGE_DURRATION_REPORT_PROJECTS').split(',').each do |project|
+      all_builds = DataFetcher.new(project).all_data.first(ENV.fetch('NUMBER_OF_BUILDS_TO_ANALIZE').to_i)
       report = CIAverageDurrationReport.new(project, all_builds.dup)
       report.present
     end
 
-    ENV.fetch('DETAILED_REPORT_FOR').split(',').each do |project|
-      all_builds = DataFetcher.new(project).all_builds_detailed.first(100)
-
-      report = CISlowestTestsReport.new(project, all_builds.dup)
+    ENV.fetch('SLOWEST_TESTS_REPORT_PROJECTS').split(',').each do |project|
+      all_builds_detailed = DataFetcher.new(project).all_builds_detailed.first(ENV.fetch('NUMBER_OF_BUILDS_TO_ANALIZE').to_i)
+      report = CISlowestTestsReport.new(project, all_builds_detailed.dup)
       report.present
+    end
 
-      report = CIFlakyTestsReport.new(project, all_builds.dup)
+    ENV.fetch('FLAKY_TESTS_REPORT_PROJECTS').split(',').each do |project|
+      all_builds_detailed = DataFetcher.new(project).all_builds_detailed.first(ENV.fetch('NUMBER_OF_BUILDS_TO_ANALIZE').to_i)
+      report = CIFlakyTestsReport.new(project, all_builds_detailed.dup)
       report.present
     end
   end
