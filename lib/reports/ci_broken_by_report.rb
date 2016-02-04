54d1146e8bc4659bb3449028f974a2524313e0e5
Ionut Alexandru Anca <ionut998@gmail.com>
2016-02-04 18:59:11 +0000

    broken by report working

--- lib/reports/ci_broken_by_report.rb
+++ lib/reports/ci_broken_by_report.rb
@@ -1,5 +1,26 @@
 class CiBrokenByReport < CIReportBase
 
   def present
+    puts project + ' - ' + passing.to_s
+
+    if !passing
+      change_set = data_fetcher_instance.get_build(breaking_build)['changeSet']['items'][0]
+      puts "commitId = #{change_set['commitId']}"
+      puts "name = #{change_set['author']['fullName']}"
+      puts "commit message = #{change_set['msg']}"
+      puts "date = #{change_set['date']}"
+    end
+  end
+
+  def overall_data
+    data_fetcher_instance.overall_data
+  end
+
+  def passing
+    overall_data['lastSuccessfulBuild']['number'].to_i > overall_data['lastUnsuccessfulBuild']['number'].to_i
+  end
+
+  def breaking_build
+    overall_data['lastStableBuild']['number'].to_i + 1
   end
 end
