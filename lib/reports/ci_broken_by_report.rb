34edcfeeff90a8cd91bf2b9830818e3197337823
Ionut Alexandru Anca <ionut998@gmail.com>
2016-02-25 13:40:36 +0000

    refactor broken by report

--- lib/reports/ci_broken_by_report.rb
+++ lib/reports/ci_broken_by_report.rb
@@ -1,30 +1,45 @@
 class CiBrokenByReport < CIReportBase
 
   def present
-    if !passing
-      change_set = data_fetcher_instance.get_build(breaking_build)['changeSet']['items'][0]
-      return if change_set.nil?
-      status = "Broken by: #{change_set['author']['fullName']}"
-    else
-      status = 'Passing'
-    end
+    # skip if breaking build cannot be found
     DashboardUpdater.new(
       "#{project}-ci-status",
       'title' => project,
-      'status' => status
+      'status' => status_message
     ).update
   end
 
-  def overall_data
+  private
+
+  def status_message
+    if !passing
+      "Broken by: #{change_set['author']['fullName']}"
+    else
+      'Passing'
+    end
+  end
+
+  def change_set
+    breaking_build['changeSet']['items'][0]
+  end
+
+  def build_overall_data
     data_fetcher_instance.overall_data
   end
 
   def passing
-    return true if overall_data.nil? || overall_data['lastUnsuccessfulBuild'].nil?
-    overall_data['lastSuccessfulBuild']['number'].to_i > overall_data['lastUnsuccessfulBuild']['number'].to_i
+    # assume that is passing if lastUnsuccessfulBuild does not exist
+    return true if build_overall_data.nil? || build_overall_data['lastUnsuccessfulBuild'].nil?
+    build_overall_data['lastSuccessfulBuild']['number'].to_i > build_overall_data['lastUnsuccessfulBuild']['number'].to_i
+  end
+
+  def breaking_build_number
+    return unless build_overall_data
+    build_overall_data['lastStableBuild']['number'].to_i + 1
   end
 
   def breaking_build
-    overall_data['lastStableBuild']['number'].to_i + 1
+    return unless breaking_build_number
+    data_fetcher_instance.get_build(breaking_build_number)
   end
 end
