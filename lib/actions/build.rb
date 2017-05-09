ad43e5bd139c297f590d59a0396095fedc134e95
David Sevcik <david.sevcik@gmail.com>
2017-05-09 17:15:32 +0200

    Extract build process functionality into separate class

---
+++ lib/actions/build.rb
@@ -0,0 +1,84 @@
+require 'date'
+
+module Build
+  class Process
+
+    def initialize(build_json:, rspec_json: nil, project: nil, upstream_strategy: :find)
+      @build_json = build_json
+      @rspec_json = rspec_json
+      @project = project || get_project_from_build_json(build_json)
+      @upstream_strategy = upstream_strategy
+    end
+
+    def execute
+      @project.add_build(build_record_attributes(@build_json, @rspec_json)).tap do |build|
+        process_rspec_json(build, @rspec_json)
+      end
+    end
+
+    private
+
+    def get_project_from_build_json(json)
+      fail 'Build json does not contain url' unless json['url'] =~ /\/job\/([^\/]+)/
+      StatsDb::Project.where(name: $1).first
+    end
+
+    def build_record_attributes(build_json, rspec_json)
+      attributes = {
+        ci_id: build_json['id'].to_i,
+        result: build_json['result'].try(:downcase),
+        document: build_json,
+        rspec_json: rspec_json,
+        timestamp: DateTime.strptime((build_json['timestamp'] / 1000).to_s, '%s'),
+      }
+      return attributes unless build_json['actions'].is_a?(Array)
+      action = build_json['actions'].find { |action| action.key?('causes') }
+      cause = action['causes'][0] if action
+      return attributes unless cause && cause['upstreamProject'] && cause['upstreamBuild']
+
+      attributes.merge(
+        upstream_project_id: upstream_project_id(cause),
+        upstream_build_id: upstream_build_id(cause),
+      )
+    end
+
+    def upstream_build_id(cause)
+      project_id = upstream_project_id(cause)
+      build_id = StatsDb::Build.where(project_id: project_id, ci_id: cause['upstreamBuild']).select_map(:id).first
+      return build_id if build_id
+      case @upstream_strategy
+      when :fetch
+        build_json = DataFetcher.new(cause['upstreamProject']).get_build(cause['upstreamBuild'])
+        upstream_build = self.class.new(build_json: build_json).execute
+        upstream_build.id
+      end
+    end
+
+    def upstream_project_id(cause)
+      @_upstream_project_id ||= begin
+        project_id = StatsDb::Project.where(name: cause['upstreamProject']).select_map(:id).first
+        return project_id if project_id
+        case @upstream_strategy
+        when :fetch
+          upstream_project = StatsDb::Project.find_or_create(name: cause['upstreamProject'])
+          upstream_project.id
+        end
+      end
+    end
+
+    def process_rspec_json(build, rspec_json)
+      return if rspec_json.blank?
+      (rspec_json['examples'] || []).each do |example|
+        spec = StatsDb::Spec.find_or_create(project: @project, file_path: example['file_path'])
+        spec_case = StatsDb::SpecCase.find_or_create(spec: spec, description: example['full_description'])
+
+        spec_case.add_spec_case_run(
+          build: build,
+          status: example['status'],
+          exception: example['exception'],
+          run_time: example['run_time'],
+        )
+      end
+    end
+  end
+end
--- lib/jobs/update_stats_db.rb
+++ lib/jobs/update_stats_db.rb
@@ -1,5 +1,5 @@
 require 'job'
-require 'date'
+require 'actions/build'
 
 
 class UpdateStatsDb < Job
@@ -14,7 +14,7 @@ class UpdateStatsDb < Job
     if builds_range
       builds_range.to_a.each do |id|
         doc = fetcher.get_build(id)
-        process_build(project, doc) if doc
+        process_build(project, doc)
       end
     else
       fetcher.each_build { |doc| process_build(project, doc) }
@@ -24,51 +24,15 @@ class UpdateStatsDb < Job
   private
 
   def process_build(project, build_json)
-    return unless build_json
-    build = project.builds_dataset.where(ci_id: build_json['id'].to_i).first
-    return build if build
-    rspec_json = DataFetcher.retrieve_rspec_json(build_json)
+    return if build_json.blank?
+    return unless project.builds_dataset.where(ci_id: build_json['id'].to_i).empty?
     print '.'
-    build = project.add_build(build_record_attributes(build_json, rspec_json))
-    process_rspec_json(project, build, rspec_json)
-    build
-  end
 
-  def build_record_attributes(build_json, rspec_json)
-    attributes = {
-      ci_id: build_json['id'].to_i,
-      result: build_json['result'].try(:downcase),
-      document: build_json,
-      rspec_json: rspec_json,
-      timestamp: DateTime.strptime((build_json['timestamp'] / 1000).to_s, '%s'),
-    }
-    return attributes unless build_json['actions'].is_a?(Array)
-    action = build_json['actions'].find { |action| action.key?('causes') }
-    cause = action['causes'][0] if action
-    return attributes unless cause && cause['upstreamProject'] && cause['upstreamBuild']
-
-    upstream_project = StatsDb::Project.find_or_create(name: cause['upstreamProject'])
-    fetcher = DataFetcher.new(cause['upstreamProject'])
-    upstream_build = process_build(upstream_project, fetcher.get_build(cause['upstreamBuild']))
-
-    attributes.merge(
-      upstream_build_id: upstream_build.try(:id),
-      upstream_project_id: upstream_project.try(:id),
-    )
-  end
-
-  def process_rspec_json(project, build, rspec_json)
-    return if rspec_json.blank?
-    (rspec_json['examples'] || []).each do |example|
-      spec = StatsDb::Spec.find_or_create(project: project, file_path: example['file_path'])
-      spec_case = StatsDb::SpecCase.find_or_create(spec: spec, description: example['full_description'])
-
-      spec_case.add_spec_case_run(
-        build: build,
-        status: example['status'],
-        exception: example['exception'],
-        run_time: example['run_time'],
-      )
-    end
+    Build::Process.new(
+      build_json: build_json,
+      rspec_json: DataFetcher.retrieve_rspec_json(build_json),
+      project: project,
+      upstream_strategy: :fetch,
+    ).execute
   end
 end
